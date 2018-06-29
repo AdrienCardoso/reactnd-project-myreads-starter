16711bf29620560305115134345d23cc5b5bddb9
Adrien Cardoso <adrien.cardoso@gmail.com>
2018-06-29 19:32:46 -0300

    feat: Created ListBooks component using BooksAPI data

---
+++ src/ListBooks.js
@@ -0,0 +1,60 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types'
+
+class ListBooks extends Component {
+  static PropTypes = {
+    books: PropTypes.array.isRequired,
+    bookShelves: PropTypes.array.isRequired
+  }
+
+  render () {
+    const { books } = this.props
+    const { bookShelves } = this.props
+
+    return (
+      <div className="list-books">
+        <div className="list-books-title">
+          <h1>MyReads</h1>
+        </div>
+        <div className="list-books-content">
+          <div>
+            {bookShelves.map((shelf) => (
+              <div className="bookshelf">
+                <h2 className="bookshelf-title">{shelf.title}</h2>
+                <div className="bookshelf-books">
+                  <ol className="books-grid">
+                    {books.map((book) => (
+                      <li key={book.id}>
+                        <div className="book">
+                          <div className="book-top">
+                            <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})` }}></div>
+                            <div className="book-shelf-changer">
+                              <select>
+                                <option value="move" disabled>Move to...</option>
+                                <option value="currentlyReading">Currently Reading</option>
+                                <option value="wantToRead">Want to Read</option>
+                                <option value="read">Read</option>
+                                <option value="none">None</option>
+                              </select>
+                            </div>
+                          </div>
+                          <div className="book-title">{book.title}</div>
+                          <div className="book-authors">{book.authors}</div>
+                        </div>
+                      </li>
+                    ))}
+                  </ol>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+        <div className="open-search">
+          <a onClick={() => this.setState({ showSearchPage: true })}>Add a book</a>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default ListBooks
